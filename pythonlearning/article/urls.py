0fdd4fc569dfa5ab87ec8ef110ac5b6001a414d4
Boris Molchanov <southsidecover@gmail.com>
2022-10-27 20:15:24 +0300

    Initial commit

---
+++ pythonlearning/article/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, re_path
+from django.views.decorators.cache import cache_page
+
+from .views import *
+
+urlpatterns = [
+    path('', ArticleHome.as_view(), name='home'),
+    path('about/', about, name='about'),
+    path('addpage/', AddPage.as_view(), name='addpage'),
+    path('contact/', ContactFormView.as_view(), name='contact'),
+    path('register/', RegisterUser.as_view(), name='register'),
+    path('login/', LoginUser.as_view(), name='login'),
+    path('logout/', logout_user, name='logout'),
+    path('post/<slug:post_slug>/', ShowPost.as_view(), name='post'),
+    path('category/<slug:cat_slug>/', ArticleCategory.as_view(), name='category'),
+
+]
